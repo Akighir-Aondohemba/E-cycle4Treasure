50a9b579dbca02715da239b1ce3e5a4110f84821
Aondohemba <akighirjoseph77@gmail.com>
2024-02-14 15:57:00 +0100

    <commit message>

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home"; // Import the Home component
+import Signin from "./pages/Signin";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Contact from "./pages/Contact";
 
 export default function App() {
   return (
-    <h1 className='text-red-500'>App</h1>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Sign-in" element={<Signin />} />
+        <Route path="/Sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
+
